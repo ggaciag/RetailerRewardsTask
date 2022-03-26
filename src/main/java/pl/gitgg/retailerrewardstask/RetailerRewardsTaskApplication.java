fe09c08224545d56c9b6bace4dc0ee4c59e4fb36
GGaciag <grzegorz.gaciag@gmail.com>
2022-03-26 22:09:29 +0100

    [Update] Project Initializr

---
+++ src/main/java/pl/gitgg/retailerrewardstask/RetailerRewardsTaskApplication.java
@@ -0,0 +1,13 @@
+package pl.gitgg.retailerrewardstask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailerRewardsTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RetailerRewardsTaskApplication.class, args);
+    }
+
+}
